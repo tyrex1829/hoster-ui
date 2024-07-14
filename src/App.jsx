1a96de91d712818b24e509244da7ddec9fde5567
tyrex1829 <sakshamkhare19@gmail.com>
2024-07-14 18:35:21 +0530

    component test code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
